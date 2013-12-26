7fdbf00754920d5fbb821ca2a6c960abdb8e28a1
Drew Blumberg <alblumberg21@gmail.com>
2013-12-25 23:25:05 -0600

    4 week month test successful

--- test/test_integration_tests_cal.rb
+++ test/test_integration_tests_cal.rb
@@ -59,18 +59,18 @@ EOS
     assert_equal shell_output, expected_output
   end
 
-#   def test_4_week_month
-#     shell_output = `ruby cal.rb 2 2015`
-#     expected_output = <<EOS
-#    February 2015
-# Su Mo Tu We Th Fr Sa
-#  1  2  3  4  5  6  7
-#  8  9 10 11 12 13 14
-# 15 16 17 18 19 20 21
-# 22 23 24 25 26 27 28
-# EOS
-#     assert_equal shell_output, expected_output
-#   end
+  def test_4_week_month
+    shell_output = `ruby cal.rb 2 2015`
+    expected_output = <<EOS
+   February 2015    
+Su Mo Tu We Th Fr Sa
+ 1  2  3  4  5  6  7
+ 8  9 10 11 12 13 14
+15 16 17 18 19 20 21
+22 23 24 25 26 27 28
+EOS
+    assert_equal shell_output, expected_output
+  end
 
 #   def test_february_in_leap_year_without_extra_day
 #     shell_output = `ruby cal.rb 2 1900`
